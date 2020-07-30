dc347c6a6baee413e2eb165b2d43e7f9327ff09d
sdimka <s_d_a@mail.ru>
2020-07-31 00:37:48 +0300

    Experiments with declare a variable in Gradle

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val a = R.string.APP_KEY_2
+    val b = BuildConfig.APP_KEY_1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
